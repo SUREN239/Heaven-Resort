97301b6c1e667dde702ab144ca2744404ca88f42
SUREN S <121883751+SUREN239@users.noreply.github.com>
2024-04-12 15:08:30 +0530

    Delete heaven directory

--- heaven/src/main/java/suren/heaven/repository/ProfileRepo.java
+++
@@ -1,9 +0,0 @@
-package suren.heaven.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import suren.heaven.model.Profile;
-
-public interface ProfileRepo extends JpaRepository<Profile, Long> {
-
-
-}
